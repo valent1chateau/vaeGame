cd642767a64a3742cf71fd35e15f260d36bbc25f
unknown <nicolas.deraisin@gmail.com>
2020-06-26 03:16:12 +0200

    Application Texture pic et torche

--- src/vae_project/Pic.java
+++ src/vae_project/Pic.java
@@ -2,18 +2,19 @@ package vae_project;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class Pic extends Wall {
-	
+	static BufferedImage image = new Resource().getResourceImage("../picture/pic.png");
 	public Pic(int x,int y, int width, int height) {
 		super(x,y,width,height);
 	}
 	
 	public void draw(Graphics2D gtd) {
-		gtd.setColor(Color.black);
-		gtd.drawRect(this.x, this.y, this.width, this.height);
-		gtd.setColor(Color.green);
-		gtd.fillRect(this.x+1, this.y+1, this.width-2, this.height-2);
-		//gtd.drawImage(image, this.x, this.y, null);
+		//gtd.setColor(Color.black);
+		//gtd.drawRect(this.x, this.y, this.width, this.height);
+		//gtd.setColor(Color.green);
+		//gtd.fillRect(this.x+1, this.y+1, this.width-2, this.height-2);
+		gtd.drawImage(image, this.x, this.y, null);
 	}
 }
--- src/vae_project/Torche.java
+++ src/vae_project/Torche.java
@@ -2,18 +2,24 @@ package vae_project;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 
 public class Torche extends Wall {
-	
+	ImageIcon iconTorche;
+	Image imageTorche;
 	public Torche(int x,int y, int width, int height) {
 		super(x,y,width,height);
+		this.iconTorche= new ImageIcon(getClass().getResource("../picture/torche.gif"));
+		this.imageTorche = this.iconTorche.getImage();
 	}
 	
 	public void draw(Graphics2D gtd) {
-		gtd.setColor(Color.black);
-		gtd.drawRect(this.x, this.y, this.width, this.height);
-		gtd.setColor(Color.red);
-		gtd.fillRect(this.x+1, this.y+1, this.width-2, this.height-2);
-		//gtd.drawImage(image, this.x, this.y, null);
+		//gtd.setColor(Color.black);
+		//gtd.drawRect(this.x, this.y, this.width, this.height);
+		//gtd.setColor(Color.red);
+		//gtd.fillRect(this.x+1, this.y+1, this.width-2, this.height-2);
+		gtd.drawImage(imageTorche, this.x, this.y, null);
 	}
 }
